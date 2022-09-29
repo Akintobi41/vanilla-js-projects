d1883d0449404201660914ba6d5639a73d8adf80
Akintobi41 <akintobi41moyinoluwa@gmail.com>
2022-09-29 23:46:30 +0100

    updated gender filter section

--- characters/characters.js
+++ characters/characters.js
@@ -1,21 +1,23 @@
-import * as _ from "../utils.js"
+import * as _ from "../utils.js";
+
 let hamToggle = _.getEl(".hamburger"),
     navigation = _.getEl(".navigation"),
-    book_titles = _.getEl('.book-titles'),
     btn_prev = _.getEl('.previous'),
     btn_next = _.getEl('.next'),
     characterSection = _.getEl('.character-section'),
     minContent = _.getEl('.min-content'),
     maxContent = _.getEl('.max-content'),
-    genderSelect = _.getEl('#gender-select')
+    genderSelect = _.getEl('#gender-select');
 
-// Hamburger Menu  // Find solution to this
+
+// Hamburger Menu 
 document.onclick = function (e) {
     if (e.target !== hamToggle && e.target !== navigation) {
-        hamToggle.classList.remove("active");
-        navigation.classList.remove("active");
+        _.removeClass(hamToggle, 'active')
+        _.removeClass(navigation, 'active')
     }
 };
+
 hamToggle.onclick = function () {
     hamToggle.classList.toggle("active");
     navigation.classList.toggle("active");
@@ -27,13 +29,12 @@ import { retrievedCharacters } from "../index.js";
 let character_docs = retrievedCharacters.docs,
     sectionData = [];
 
-character_docs.forEach((val, i) => _.characterCard(val, i, sectionData))
+character_docs.forEach((val, i) => _.characterCard(val, i, sectionData));
 
 // Pagination
 let exactPage = 1;
 
-//Adding event listeners
-function navigate(element, event, fnName) { element.addEventListener(event, fnName) }
+// Adding event listeners
 
 function changePage(records_per_page, current_page, content, wrapper) {
     wrapper.innerHTML = '';
@@ -45,15 +46,17 @@ function changePage(records_per_page, current_page, content, wrapper) {
 
     paginatedItems.forEach((items) => wrapper.append(items));   // Appending items to DOM
 
-    ((current_page + 1) != 1) ? btn_prev.classList.remove("previous-button") :
-        _.addClass(btn_prev, "previous-button")
+    ((current_page + 1) != 1) ?
+        btn_prev.classList.remove("previous-button") :
+        _.addClass(btn_prev, "previous-button");
+
+    ((current_page + 1) === numPages(content, records_per_page)) ?
+        _.addClass(btn_next, 'next-button') :
+        _.removeClass(btn_next, 'next-button')
 
     minContent.textContent = end;
     maxContent.textContent = content.length;
     (end > content.length) ? minContent.textContent = content.length : false
-
-
-
 }
 
 btn_next.addEventListener('click', callsNext)
@@ -61,7 +64,7 @@ btn_prev.addEventListener('click', callsPrevious)
 
 
 function callsNext() {
-    exactPage++;
+    if (exactPage < numPages(sectionData, 10)) exactPage++;
     next(exactPage, sectionData, characterSection)
 }
 
@@ -69,8 +72,6 @@ function callsPrevious() {
     if (exactPage > 1) exactPage--;
     previous(exactPage, sectionData, characterSection)
 }
-
-
 
 function numPages(content, records_per_page) {
     return Math.ceil(content.length / records_per_page)
@@ -87,86 +88,27 @@ function previous(page, data, wrapper) {
     (page >= 1) ?
         (changePage(10, page, data, wrapper)) : false
 }
-
 //
 
-
 // Filter by Gender
-
 genderSelect.addEventListener('change', genderUI)
-let exactPage1 = 1;
-
-// const filterItems = (gender) => {
-//     const genders = ['males', 'male', 'females']
-
-//     const newArr = [...sectionData].filter((item) => {
-//         const p_gender = item.children[1].textContent.toLowerCase().slice(8)
-//         if (!gender) return !genders.includes(p_gender);
-//         return p_gender === gender || p_gender === (gender + 's')
-
-//     })
-
-//     changePage(10, exactPage1, genderArr_male, characterSection)
-// }
-
-
-let genderArr_male = sectionData.filter((item) => {
-    const p_gender = item.children[1].textContent.toLowerCase().slice(8)
-    return p_gender === 'male' || p_gender === 'males'
-}),
-    genderArr_female = sectionData.filter((item) => {
-        const p_gender = item.children[1].textContent.toLowerCase().slice(8)
-        return p_gender === 'female'
-    }),
-    genderArr_others = sectionData.filter((item) => {
-        const p_gender = item.children[1].textContent.toLowerCase().slice(8);
-        return p_gender != 'male' && p_gender != 'female' && p_gender != 'males'
-    });
-
-function maleGender(e) {
-    (e.target.value.toLowerCase() === 'male') ? (
-        changePage(10, exactPage1, genderArr_male, characterSection))
-        : false;
-}
-
-function femaleGender(e) {
-    (e.target.value.toLowerCase() === 'female') ? (
-        changePage(10, exactPage1, genderArr_female, characterSection))
-        : false;
-}
-
-function otherGender(e) {
-    (e.target.value.toLowerCase() != 'female'
-        && e.target.value.toLowerCase() != 'male') ? (
-        changePage(10, exactPage1, genderArr_others, characterSection))
-        : false;
-}
-
-function anyGender(e) {
-    (e.target.value.toLowerCase() === 'any') ?
-        (changePage(10, exactPage1, sectionData, characterSection)) : false
-}
 
 function genderUI(e) {
-    // btn_next.removeEventListener('click', callsNext);
-    // btn_prev.removeEventListener('click', callsPrevious);
+    const gender = e.target.value;
+    const knownGenders = ['Males', 'male', 'Male', 'Female']
 
+    if (gender === 'Any') return [...character_docs];
 
-
-    maleGender(e);
-    femaleGender(e);
-    otherGender(e);
-    anyGender(e);
-
-    // filterItems(e)
-
-    // function next1() {
-    //     (exactPage1 < numPages(genderArr_male, 10)) ?
-    //         (exactPage1++, changePage(10, exactPage1, genderArr_male, characterSection)) : false
-    // }
-
-    // function previous1() {
-    //     (exactPage1 > 1) ? (exactPage1--,
-    //         changePage(10, exactPage1, genderArr_male, characterSection)) : false
-    // }
+    const newArr = [...character_docs].filter((val) => {
+        if (gender === 'Other') {
+            return !knownGenders.includes(val.gender)
+        }
+        return gender === val.gender || gender === (val.gender + 's')
+    })
+    sectionData = [];
+    exactPage = 1
+    newArr.forEach((val, i) => _.characterCard(val, i, sectionData))
+    changePage(10, 1, sectionData, characterSection)
 }
+//
+
--- utils.js
+++ utils.js
@@ -1,47 +1,63 @@
 const gender = {
     male: "/Images/man.svg",
     female: "/Images/woman.svg",
-    unknown: ''
+    unknown: "/Images/icons8-gender (1).svg"
 }
 
 export const el = (e) => document.createElement(e)
 
 export const addClass = (el, name) => el.classList.add(name)
 
+export const removeClass = (el, name) => el.classList.remove(name)
+
 export const append = (el, child) => el.appendChild(child)
 
 export const getEl = (e) => document.querySelector(e)
 
 export const characterCard = (hero, i, arr) => {
-
     const newSection = el('section'), header = el('header'),
         img = el('img'), h2 = el('h2'), p_gender = el('p'),
         p_birth = el('p'), p_race = el('p'), p_death = el('p'),
         p_spouse = el('p'), header_section = el('div'),
-        btnSection = el('button'), starImg = el('img'), p_more = el('p');
-    //card character name
+        btnSection = el('button'), starImg = el('img'), p_more = el('p'), p_realm = el('p'),
+        p_hair = el('p');
+
+    // card character name
     newSection.id = i
     h2.innerHTML = `<a href="${hero.wikiUrl}" 
         title="click to learn more about ${hero.name}"> 
         ${hero.name}</a>`;
 
-    //favorites button
+    // favorites button
     starImg.src = '/Images/star-sharp.svg';
     addToFav(btnSection, "click");
+    hoverFav(btnSection, "mouseover");
+    //
+    img.src = gender[hero.gender && hero.gender?.toString() !== "NaN" ?
+        hero.gender === "Female" ? 'female' : 'male' :
+        'unknown'];
 
-    img.src = gender[hero.gender ? hero.gender === "Female" ? 'female' : 'male' : 'unknown']
-    if (hero.gender === "Female") addClass(img, "female")
-
-    // Adding attributes for  the images
+    if (hero.gender === "Female") addClass(img, "female");
+    if (!(hero.gender?.endsWith('le' || 'les'))) addClass(img, 'unknown');
+    // Adding attributes for the images
     img.setAttribute('alt', "gender-type")
     starImg.setAttribute('alt', 'add to favorites')
-
-    p_birth.textContent = `Birth: ${hero.birth || 'Data unavailable'}`
-    p_gender.textContent = `Gender: ${hero.gender || 'Data unavailable'}`;
-    p_race.textContent = `Race: ${hero.race || 'Data unavailable'}`;
-    p_death.textContent = `Death: ${hero.death || 'Data unavailable'}`
-    p_spouse.textContent = `Spouse: ${hero.spouse || 'Data unavailable'}`
+    //
+    p_birth.textContent = `Birth: ${(hero.birth && hero.gender.toString() != 'NaN') ?
+        hero.gender : 'unknown'}`
+    p_gender.textContent = `Gender: ${(hero.gender && hero.gender.toString() != 'NaN') ?
+        hero.gender : 'unknown'}`;
+    p_race.textContent = `Race: ${(hero.race && hero.race.toString() != 'NaN') ?
+        hero.race : 'unknown'}`;
+    p_death.textContent = `Death: ${(hero.death && hero.death.toString() != 'NaN') ?
+        hero.death : 'unknown'}`
+    p_spouse.textContent = `Spouse: ${(hero.spouse && hero.spouse.toString() != 'NaN') ?
+        hero.spouse : 'unknown'}`
     p_more.textContent = 'View more...'
+    p_realm.textContent = `Realm: ${(hero.realm && hero.realm.toString() != 'NaN') ?
+        hero.realm : 'unknown'}`
+    p_hair.textContent = `Hair: ${(hero.hair && hero.hair.toString() != 'NaN') ?
+        hero.hair : 'unknown'}`
 
     append(header_section, img)
     append(header_section, h2)
@@ -55,7 +71,8 @@ export const characterCard = (hero, i, arr) => {
     append(newSection, p_death)
     append(newSection, p_spouse)
     append(newSection, p_more)
-
+    append(newSection, p_realm)
+    append(newSection, p_hair)
     // Adding class names to elements
     addClass(newSection, "character-section-items")
     addClass(header, "character-section-header")
@@ -70,18 +87,30 @@ export const characterCard = (hero, i, arr) => {
     addClass(starImg, 'star-img')
     addClass(btnSection, 'btn-section')
     addClass(p_more, "view-more-text")
+    addClass(p_realm, 'realm-text')
+    addClass(p_hair, 'hair-text');
 
+    p_more.addEventListener('click', function () {
+        viewMore(p_realm)
+        viewMore(p_hair)
+        addClass(p_more, "more-info")
+    })
     arr = arr.push(newSection)
 }
 
+const viewMore = (element) => element.style.display = "block";
 
 export function addToFav(element, listener) {
-    element.setAttribute("title", "Add to favorites")
     element.addEventListener(listener, function () {
         element.classList.toggle('active');
-        element.setAttribute('title', "Added to Favorites")
+    });
+}
+
+export function hoverFav(element, listener) {
+    const e_class = element.classList;
+    element.addEventListener(listener, function () {
+        (e_class[e_class.length - 1].includes('active')) ?
+            element.setAttribute('title', 'Remove from Favorites') :
+            element.setAttribute('title', 'Add to Favorites')
     })
-    if (!element.classList.value.includes('active')) element.removeAttribute('title')
-
-
 }
